91cb82a09689065801b8f27d2e2d11403fefa78b
Rg4liff <79674066+Rg4liff@users.noreply.github.com>
2025-02-14 16:53:33 -0600

    New Chatbot Project WIP

---
+++ Geocode_parse.py
@@ -0,0 +1,36 @@
+import requests
+import os
+import json
+from dotenv import load_dotenv
+
+
+load_dotenv()
+geocode_token = os.getenv('GEOCODE_TOKEN')
+Geocode_base_url = 'https://maps.googleapis.com/maps/api/geocode/json?'
+
+
+def GetGeocode(address):
+    url = f'{Geocode_base_url}address={address}&key={geocode_token}'
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        geocode_data = response.json()
+        return geocode_data
+    else:
+        print(f'Error: Failed to retrieve data {response.status_code}')
+
+
+def GetCoords(addr):
+    address = addr.replace(' ', '+')
+    print(address)
+    info = GetGeocode(address)
+    print(json.dumps(info, indent=4))
+    if info:
+        coords = f'{info["results"][0]["geometry"]["location"]["lat"]},{info["results"][0]["geometry"]["location"]["lng"]}'
+        return coords
+
+# check if this works first and retrieves prop coords before implementing
+
+#test_address = '1609 S 33rd St, Omaha, NE'
+#test_coords = GetCoords(test_address)
+#print(test_coords)
